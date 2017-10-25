62964824df04f6d34e5fb8d57cec0cd10da51091
James Schmidt <jschmidt19@jcu.edu>
2017-10-25 20:24:36 +0000

    images working on fractals

--- app/controllers/fractal_controller.rb
+++ app/controllers/fractal_controller.rb
@@ -8,10 +8,16 @@ class FractalController < ApplicationController
       end
       
       
+      
       if params[:individual] != nil 
           @individual = params[:individual]
+          
       else
           @individual = "fractal1";
+      end
+      @fractal = []
+      File.open("/home/codio/workspace/depot/app/assets/images/Fractals/" + @individual + ".txt").each do |line|
+        @fractal.append(line)
       end
       
   end
